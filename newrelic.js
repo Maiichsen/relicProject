4835b5c6b24f46d3392581e17566f6fb7ab10753
Mai Jockwich <mmjockwich@gmail.com>
2025-03-24 12:44:10 +0100

    first commit

---
+++ newrelic.js
@@ -0,0 +1,27 @@
+'use strict'
+
+exports.config = {
+  app_name: ['relicTest'],
+  license_key: 'eu01xx6b3c624028d3427043890a3001FFFFNRAL',
+  distributed_tracing: {
+    enabled: true
+  },
+  logging: {
+    level: 'info'
+  },
+  allow_all_headers: true,
+  attributes: {
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*'
+    ]
+  }
+}
